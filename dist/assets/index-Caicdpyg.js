a18dcfe4c3ae66382985307a7a3c092c933b33f8
00mark0 <00marko.r@gmail.com>
2024-05-10 01:02:40 +0200

    ran the build

--- dist/assets/index-Caicdpyg.js
+++ dist/assets/index-Caicdpyg.js
@@ -46,8 +46,8 @@ var sn=(r,e)=>()=>(e||r((e={exports:{}}).exports,e),e.exports);var Un=sn((Bn,De)
     </div>
     <div class="buttons">
         <button id="rotate">Rotate</button>
-        <button id="start">Start Game</button>
         <button id="randomize" title="Press Q on the keyboard to place ships randomly.">Randomize</button>
+        <button id="start">Start Game</button>
         <button id="restart" title="Press C on the keyboard to restart the setup page.">Restart</button>
         <button id="back-to-menu" title="Press Escape on the keyboard to go back to intro screen.">Back</div> 
     </div>
