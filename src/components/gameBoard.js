00b3042dce4e2c6c59c89856d6e5542851f75f32
00mark0 <00marko.r@gmail.com>
2024-05-09 23:46:23 +0200

    added additional info when placing ships

--- src/components/gameBoard.js
+++ src/components/gameBoard.js
@@ -1,3 +1,5 @@
+import initDomElements from "./domElements";
+
 // battleship game board class
 
 export class GameBoard {
@@ -7,24 +9,51 @@ export class GameBoard {
   }
 
   placeShip(ship, startPosition, direction) {
+    let domElements = initDomElements();
     const shipLength = ship.length;
     let [startRow, startCol] = startPosition; // assuming startPosition is an array [row, col]
 
     for (let i = 0; i < shipLength; i++) {
       if (direction === "horizontal") {
         if (startCol + i >= 10) {
-          throw new Error("Ship placement out of bounds.");
+          domElements.gameInfo.textContent = "Ship placement out of bounds.";
+          domElements.gameInfo.style.display = "block";
+
+          setTimeout(function () {
+            domElements.gameInfo.textContent = "";
+            domElements.gameInfo.style.display = "none";
+          }, 3000);
         }
         if (this.board[startRow][startCol + i] !== null) {
-          throw new Error("Position already occupied.");
+          domElements.gameInfo.textContent = "Position already occupied.";
+          domElements.gameInfo.style.display = "block";
+          domElements.ship[shipIndex.value].style.display = "block";
+
+          setTimeout(function () {
+            domElements.gameInfo.textContent = "";
+            domElements.gameInfo.style.display = "none";
+          }, 3000);
         }
         this.board[startRow][startCol + i] = ship; // place part of the ship
       } else if (direction === "vertical") {
         if (startRow + i >= 10) {
-          throw new Error("Ship placement out of bounds.");
+          domElements.gameInfo.textContent = "Ship placement out of bounds.";
+          domElements.gameInfo.style.display = "block";
+
+          setTimeout(function () {
+            domElements.gameInfo.textContent = "";
+            domElements.gameInfo.style.display = "none";
+          }, 3000);
         }
         if (this.board[startRow + i][startCol] !== null) {
-          throw new Error("Position already occupied.");
+          domElements.gameInfo.textContent = "Position already occupied.";
+          domElements.gameInfo.style.display = "block";
+          domElements.ship[shipIndex.value].style.display = "block";
+
+          setTimeout(function () {
+            domElements.gameInfo.textContent = "";
+            domElements.gameInfo.style.display = "none";
+          }, 3000);
         }
         this.board[startRow + i][startCol] = ship; // place part of the ship
       }
